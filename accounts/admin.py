579ca42cc197bed3ae84c3bc7c4a135ddedc7c08
Vikhil <vikhil.sallagargi@easiloan.com>
2026-06-24 23:16:10 +0530

    password reset

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,22 +3,14 @@ from django import forms
 from .models import User, HaLogins
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-class CustomUserCreationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
-
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('email',)
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
-
-class CustomUserChangeForm(forms.ModelForm):
+class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = '__all__'
